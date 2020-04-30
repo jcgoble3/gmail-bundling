5dbb7d0877d40fcb5edd1d2365912ffde313747e
Jonathan Goble <jcgoble3@gmail.com>
2020-04-29 22:03:51 -0400

    More reminder stuff

--- src/options/script.js
+++ src/options/script.js
@@ -1,18 +1,19 @@
-const REMINDER_TREATMENT_SELECTOR = 'input[name=reminder-treatment]';
+//const REMINDER_TREATMENT_SELECTOR = 'input[name=reminder-treatment]';
 const BUNDLED_EMAIL_SELECTOR = 'input[name=email-bundling]';
 
 function saveOptions() {
-	const reminderTreatment = getSelectedRadioValue(REMINDER_TREATMENT_SELECTOR);
+	//const reminderTreatment = getSelectedRadioValue(REMINDER_TREATMENT_SELECTOR);
 	const emailBundling = getSelectedRadioValue(BUNDLED_EMAIL_SELECTOR);
 
-	const options = { reminderTreatment, emailBundling };
+	//const options = { reminderTreatment, emailBundling };
+	const options = { emailBundling };
 
 	localStorage.setItem('options', JSON.stringify(options));
 }
 
 function restoreOptions() {
 	chrome.runtime.sendMessage({ method: 'getOptions' }, function(options) {
-		selectRadioWithValue(REMINDER_TREATMENT_SELECTOR, options.reminderTreatment);
+		//selectRadioWithValue(REMINDER_TREATMENT_SELECTOR, options.reminderTreatment);
 		selectRadioWithValue(BUNDLED_EMAIL_SELECTOR, options.emailBundling);
 	});
 }
@@ -28,5 +29,5 @@ const getSelectedRadioValue = selector => document.querySelector(selector + ':ch
 const monitorChange = element => element.addEventListener('click', saveOptions);
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
-document.querySelectorAll(REMINDER_TREATMENT_SELECTOR).forEach(monitorChange);
+//document.querySelectorAll(REMINDER_TREATMENT_SELECTOR).forEach(monitorChange);
 document.querySelectorAll(BUNDLED_EMAIL_SELECTOR).forEach(monitorChange);
